eb6432b1464851503a6631fbd6617a8da9412fcc
XP <xp@codeborne.com>
2012-08-22 12:01:32 +0300

    common functionality extracted to module

---
+++ app/play/test/TwitterBootstrapUITest.java
@@ -0,0 +1,42 @@
+package play.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static com.codeborne.selenide.DOM.assertElement;
+import static com.codeborne.selenide.DOM.getElement;
+import static com.codeborne.selenide.DOM.waitUntil;
+import static com.codeborne.selenide.Condition.*;
+
+import static org.openqa.selenium.Keys.ESCAPE;
+
+public class TwitterBootstrapUITest extends UITest {
+  protected void closeModal(By by) {
+    getElement(By.tagName("body")).sendKeys(ESCAPE);
+    waitUntil(by, hidden);
+  }
+
+  protected void assertSuccessMessage(String message) {
+    assertElement(By.className("alert-success"), hasText(message));
+  }
+
+  protected void assertWarningMessage(String message) {
+    assertElement(By.className("alert-warning"), hasText(message));
+  }
+
+  protected void assertInfoMessage(String message) {
+    assertElement(By.className("alert-info"), hasText(message));
+  }
+
+  protected void assertErrorMessage(String message) {
+    assertElement(By.className("alert-error"), hasText(message));
+  }
+
+  @SuppressWarnings("StatementWithEmptyBody")
+  protected void assertFieldHasError(String name, String message) {
+    WebElement element = getElement(By.name(name));
+    while (!(element = element.findElement(By.xpath(".."))).getAttribute("class").contains("control-group"));
+    if (!element.getAttribute("class").contains("error") && !element.findElement(By.className("error")).isDisplayed()) fail("No error class on " + element.getText());
+    assertElement(element, hasText(label(message)));
+  }
+}
---
+++ app/play/test/UITest.java
@@ -0,0 +1,51 @@
+package play.test;
+
+import com.codeborne.selenide.Navigation;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.BeforeClass;
+import play.Play;
+import play.i18n.Messages;
+import play.mvc.After;
+import play.mvc.Router;
+import play.server.Server;
+
+import static com.codeborne.selenide.DOM.executeJavaScript;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+public abstract class UITest extends BaseTest {
+  private static boolean serverStarted = false;
+
+  @BeforeClass
+  public static synchronized void startServer() throws Exception {
+    if (!serverStarted) {
+      new Server(new String[]{});
+      serverStarted = true;
+    }
+    Navigation.baseUrl = "http://localhost:" + Play.configuration.get("http.port");
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    if (Boolean.getBoolean("screenshots"))  WebDriverRunner.takeScreenShot("" + System.currentTimeMillis());
+  }
+
+  protected String label(String key) {
+    return Messages.get(key);
+  }
+
+  protected String label(String key, Object... args) {
+    return Messages.get(key, args);
+  }
+
+  protected void assertAction(String action) {
+    assertEquals(Router.getFullUrl(action), getWebDriver().getCurrentUrl().replaceFirst("\\?.*$", ""));
+  }
+
+  protected void mockConfirm() {
+    executeJavaScript("window.confirm = function() {return true;};");
+  }
+
+  protected void mockAlert() {
+    executeJavaScript("window.confirm = function() {};");
+  }
+}
