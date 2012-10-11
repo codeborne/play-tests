04459f2ab94bc88a7652be9ebe316da17ae3b0b4
Anton Keks <anton@codeborne.com>
2012-10-11 17:18:49 +0300

    reverting static methods - they must be overridable

--- app/play/test/UITest.java
+++ app/play/test/UITest.java
@@ -32,23 +32,23 @@ public abstract class UITest extends BaseTest {
     }
   };
 
-  protected static String label(String key) {
+  protected String label(String key) {
     return Messages.get(key);
   }
 
-  protected static String label(String key, Object... args) {
+  protected String label(String key, Object... args) {
     return Messages.get(key, args);
   }
 
-  protected static void assertAction(String action) {
+  protected void assertAction(String action) {
     assertEquals(Router.getFullUrl(action), getWebDriver().getCurrentUrl().replaceFirst("\\?.*$", ""));
   }
 
-  protected static void mockConfirm() {
+  protected void mockConfirm() {
     executeJavaScript("window.confirm = function() {return true;};");
   }
 
-  protected static void mockAlert() {
+  protected void mockAlert() {
     executeJavaScript("window.confirm = function() {};");
   }
 }
