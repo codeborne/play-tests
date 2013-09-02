75a1949b5c084909469de132313025a1038b44f5
Andrei Solntsev <andrei@codeborne.com>
2013-09-02 18:58:36 +0300

    Fixed name of zip file

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+MODULE="selenide"
+VERSION=`grep self conf/dependencies.yml | sed "s/.*$MODULE //"`
+
 rm -fr dist
 play dependencies --sync
 play build-module
-#scp dist/play-selenide-*.zip xp@codeborne.com:/var/www/repo/play-selenide/
-cp dist/play-selenide-*.zip /var/www/repo/play-selenide/
+#scp dist/*.zip xp@codeborne.com:/var/www/repo/play-selenide/$MODULE-$VERSION.zip
+cp dist/*.zip /var/www/repo/play-selenide/$MODULE-$VERSION.zip
