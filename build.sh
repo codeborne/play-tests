134b984c9b592dd73a33b6d46ab4266b3cbe2149
Andrei Solntsev <andrei.solntsev@gmail.com>
2013-09-02 23:10:00 +0300

    tuned script

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ VERSION=`grep self conf/dependencies.yml | sed "s/.*$MODULE //"`
 rm -fr dist
 play dependencies --sync
 play build-module
-#scp dist/*.zip xp@codeborne.com:/var/www/repo/play-selenide/$MODULE-$VERSION.zip
-cp dist/*.zip /var/www/repo/play-selenide/$MODULE-$VERSION.zip
+#scp dist/*.zip codeborne.com:/var/www/repo/play-$MODULE/$MODULE-$VERSION.zip
+cp dist/*.zip /var/www/repo/play-$MODULE/$MODULE-$VERSION.zip
