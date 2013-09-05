386e8589cddbeeac2862427adec0b69e37514407
Anton Keks <anton@codeborne.com>
2013-09-05 16:57:15 +0300

    event more generalized test-runner, bumping to 2.3.5

--- test-functions.sh
+++ test-functions.sh
@@ -7,23 +7,23 @@ function modify_test_env() {
 }
 
 function prepare_test_env() {
-  CLASSPATH="lib/*:$TARGET/play/framework/*:$TARGET/play/framework/lib/*"
-  CLASSES=`find app -name '*.java'`
+  TEST_CLASSPATH="lib/*:"
+  TEST_CLASSES=`find app -name '*.java'`
   for m in `ls modules`; do
     if [ -d modules/$m ]; then
-      CLASSPATH=$CLASSPATH:`pwd`/modules/$m/lib/*
-      CLASSES="$CLASSES `find modules/$m/app -name '*.java' 2>/dev/null`"
+      TEST_CLASSPATH=$TEST_CLASSPATH:`pwd`/modules/$m/lib/*
+      TEST_CLASSES="$TEST_CLASSES `find modules/$m/app -name '*.java' 2>/dev/null`"
     fi
     if [ -f modules/$m ]; then
       dir=`cat modules/$m`
-      CLASSPATH=$CLASSPATH:$dir/lib/*
-      CLASSES="$CLASSES `find $dir/app -name '*.java' 2>/dev/null`"
+      TEST_CLASSPATH=$TEST_CLASSPATH:$dir/lib/*
+      TEST_CLASSES="$TEST_CLASSES `find $dir/app -name '*.java' 2>/dev/null`"
     fi
   done
-  CLASSES="$CLASSES `find test -name '*.java'`"
+  TEST_CLASSES="$TEST_CLASSES `find test -name '*.java'`"
 
-  export CLASSPATH
-  export CLASSES
+  export TEST_CLASSPATH=$TEST_CLASSPATH:test:"$PLAY_HOME/framework/*:$PLAY_HOME/framework/lib/*"
+  export TEST_CLASSES=$TEST_CLASSES
 
   modify_test_env
 }
@@ -33,7 +33,7 @@ function compile_tests() {
   prepare_test_env
 
   rm -fr test-classes && mkdir test-classes
-  javac -g -source 1.7 -target 1.7 -cp $CLASSPATH $CLASSES -d test-classes || exit 1
+  javac -g -source 1.7 -target 1.7 -cp $TEST_CLASSPATH $TEST_CLASSES -d test-classes || exit 1
 }
 
 function run_unit_tests() {
@@ -41,18 +41,23 @@ function run_unit_tests() {
   prepare_test_env
   mkdir -p test-result
   TESTS_FILE=`pwd`/test-result/unit-tests.txt
-  find test-classes -name '*Test.class' | LC_ALL=C sort | sed 's@test-classes/@@; s@\.class$@@; s@/@.@g' | egrep -v '^(ui\.|play.test\.|realworld\.|controllers\.ControllerTest)' \
-    | sed "s@\(.*\)@\1,$(pwd)/test-result,\1@" > $TESTS_FILE
-  TEST_CLASSPATH=$APPDIR/test-classes:$APPDIR/test:test:$CLASSPATH
+  if [ ! -e $TESTS_FILE ]; then
+    find test-classes -name '*Test.class' | LC_ALL=C sort | sed 's@test-classes/@@; s@\.class$@@; s@/@.@g' | egrep -v '^(ui\.|play.test\.|realworld\.|controllers\.ControllerTest)' \
+      | sed "s@\(.*\)@\1,$(pwd)/test-result,\1@" > $TESTS_FILE
+  fi
 
-  java -Xmx512m -XX:-UseSplitVerifier -cp $TEST_CLASSPATH helpers.JenkinsTestRunner $TESTS_FILE || exit 666
+  java -Xmx512m -XX:-UseSplitVerifier -cp test-classes:$TEST_CLASSPATH helpers.JenkinsTestRunner $TESTS_FILE || exit 666
   echo "Finished unit tests."
 }
 
 function run_ui_tests() {
   echo "Running UI tests... "
   prepare_test_env
-  TEST_CLASSPATH=$APPDIR/test-classes:$APPDIR/test:test:$CLASSPATH
-  java -Xmx512m -XX:MaxPermSize=128m -XX:-UseSplitVerifier -Dprecompiled=true -Dbrowser=chrome -Dselenide.reports=test-result -cp $TEST_CLASSPATH helpers.JenkinsTestRunner test-result/ui-tests.txt || exit $?
+  TESTS_FILE=`pwd`/test-result/unit-tests.txt
+  if [ ! -e $TESTS_FILE ]; then
+    find test-classes -name '*Spec.class' | LC_ALL=C sort | sed 's@test-classes/@@; s@\.class$@@; s@/@.@g' \
+      | sed 's/\(.*\)/\1,test-result,\1/' > $TESTS_FILE
+  fi
+  java -Xmx512m -XX:MaxPermSize=128m -XX:-UseSplitVerifier -Dprecompiled=true -Dbrowser=chrome -Dselenide.reports=test-result -cp test-classes:$TEST_CLASSPATH helpers.JenkinsTestRunner $TESTS_FILE || exit $?
   echo "Finished UI tests."
 }
