78b96a43e41676d4717a4394ab0ca2c671f82d2a
Andrei Solntsev <andrei.solntsev@gmail.com>
2013-09-06 22:24:43 +0300

    Better message

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 MODULE="selenide"
 VERSION=`grep self conf/dependencies.yml | sed "s/.*$MODULE //"`
+TARGET=/var/www/repo/play-$MODULE/$MODULE-$VERSION.zip
 
 rm -fr dist
 play dependencies --sync
 play build-module
-#scp dist/*.zip codeborne.com:/var/www/repo/play-$MODULE/$MODULE-$VERSION.zip
 
-TARGET=/var/www/repo/play-$MODULE/$MODULE-$VERSION.zip
 if [ -e $TARGET ]; then
-    echo "Not publishing, $VERSION already exists"
+    echo "Not publishing, $MODULE-$VERSION already exists"
 else
     cp dist/*.zip $TARGET
 fi
