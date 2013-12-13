18f6cd60c9488e230b2e36ac05517f2412e3888c
Andrei Solntsev <andrei@codeborne.com>
2013-12-14 00:17:35 +0200

    Executes JUnit tests implicitly without generating a special txt files with list of tests

--- src/play/test/JUnitRunnerWithXMLOutput.java
+++ src/play/test/JUnitRunnerWithXMLOutput.java
@@ -1,15 +1,13 @@
 package play.test;
 
 import com.google.common.base.Predicate;
-import org.apache.commons.io.FileUtils;
+import org.apache.tools.ant.taskdefs.optional.junit.JUnitTest;
 import org.apache.tools.ant.taskdefs.optional.junit.JUnitTestRunner;
+import org.apache.tools.ant.taskdefs.optional.junit.SummaryJUnitResultFormatter;
 import org.apache.tools.ant.taskdefs.optional.junit.XMLJUnitResultFormatter;
 import play.Play;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.*;
 
 import static com.google.common.collect.Collections2.filter;
@@ -24,12 +22,18 @@ public class JUnitRunnerWithXMLOutput {
     }
   }
 
-  private static void prepareTestResult() throws IOException {
-    File testResults = Play.getFile("test-result");
-    if (testResults.exists()) {
-      FileUtils.deleteDirectory(testResults);
-    }
-    testResults.mkdir();
+  private final File testResults;
+  private final TestType testType;
+
+  private boolean stackfilter = true;
+  private boolean haltOnError = false;
+  private boolean haltOnFail = false;
+  private boolean showOutput = true;
+  private boolean logTestListenerEvents = false;
+
+  public JUnitRunnerWithXMLOutput(TestType testType) {
+    this.testType = testType;
+    testResults = Play.getFile("test-result");
   }
 
   private static Collection<Class> getTestClasses(List<Class> classes, TestType testType) {
@@ -51,17 +55,9 @@ public class JUnitRunnerWithXMLOutput {
 
   private static final ClassNameComparator classNameComparator = new ClassNameComparator();
 
-  private static File prepareTestsFile(TestType testType) throws IOException, ClassNotFoundException {
+  private static Collection<Class> findTestClasses(TestType testType) throws IOException, ClassNotFoundException {
     JavaSourcesCollection sources = new JavaSourcesCollection().scan();
-//    Compiler.compile(sources.getSourceRoots(), sources.getSourceFiles(), "test-classes");
-
-    Collection<Class> classes = getTestClasses(sources.getClasses(), testType);
-    List<Class> sorted = new ArrayList<Class>(classes);
-    Collections.sort(sorted, classNameComparator);
-
-    System.out.println("Run " + classes.size() + " " + testType + " tests");
-
-    return createTestsFile(testType, sorted);
+    return getTestClasses(sources.getClasses(), testType);
   }
 
   private static File createTestsFile(TestType testType, List<Class> testClasses) throws IOException {
@@ -78,11 +74,97 @@ public class JUnitRunnerWithXMLOutput {
     return file;
   }
 
+  private int runTests() throws Exception {
+    List<Class> classes = sort(findTestClasses(testType));
+
+    System.out.println("Run " + classes.size() + " " + testType + " tests");
+
+    List<JUnitTest> results = new ArrayList<JUnitTest>(classes.size());
+    for (Class testClass : classes) {
+      results.add(executeTest(testClass));
+    }
+
+    printLongestTests(results);
+    return printSummary(results);
+  }
+
+  private List<Class> sort(Collection<Class> classes) {
+    List<Class> sorted = new ArrayList<Class>(classes);
+    Collections.sort(sorted, classNameComparator);
+    return sorted;
+  }
+
+  private void printLongestTests(List<JUnitTest> results) {
+    Collections.sort(results, new Comparator<JUnitTest>() {
+      @Override public int compare(JUnitTest o1, JUnitTest o2) {
+        return Long.compare(o2.getRunTime(), o1.getRunTime());
+      }
+    });
+
+    System.out.println();
+    System.out.println();
+    System.out.println("Longest tests:");
+
+    final List<JUnitTest> top = results.size() < 10 ? results : results.subList(0, 10);
+    for (JUnitTest result : top) {
+      System.out.println(result.getName() + " -> " + result.getRunTime() + " ms.");
+    }
+  }
+
+  private int printSummary(List<JUnitTest> results) {
+    int errors = 0;
+    for (JUnitTest result : results) {
+      if (result.errorCount() != 0 || result.failureCount() != 0) {
+        if (errors == 0) {
+          System.out.println();
+          System.out.println();
+          System.out.println("Failed tests:");
+        }
+
+        System.out.println(result.getName() + " " +
+            result.errorCount() + " errors, " +
+            result.failureCount() + " failures, " +
+            result.runCount() + " runs");
+        errors += result.errorCount() + result.failureCount();
+      }
+    }
+
+    System.out.println();
+    return errors;
+  }
+
+  private JUnitTest executeTest(Class testClass) throws FileNotFoundException {
+    JUnitTest t = new JUnitTest(testClass.getName());
+    t.setTodir(testResults);
+    t.setOutfile(new File(testResults, testClass.getName() + ".xml").getAbsolutePath());
+    t.setProperties(System.getProperties());
+    t.setFork(true);
+
+    JUnitTestRunner runner = new JUnitTestRunner(t, null, haltOnError, stackfilter, haltOnFail, showOutput, logTestListenerEvents);
+    XMLJUnitResultFormatter resultFormatter = new XMLJUnitResultFormatter();
+    resultFormatter.setOutput(new BufferedOutputStream(new FileOutputStream(t.getOutfile())));
+    runner.addFormatter(resultFormatter);
+
+    final SummaryJUnitResultFormatter summary = new SummaryJUnitResultFormatter();
+    summary.setOutput(System.out);
+    runner.addFormatter(summary);
+
+//      final PlainJUnitResultFormatter plain = new PlainJUnitResultFormatter();
+//      plain.setOutput(System.out);
+//      runner.addFormatter(plain);
+
+    runner.run();
+    return t;
+  }
+
+  private static void useAntRunnerToOutputResultsIntoXml(TestType testType, String[] args) throws Exception {
+//    File testsFile = args.length == 1 ? prepareTestsFile(testType) : new File(args[1]);
+//    JUnitTestRunner.main(new String[]{"testsfile=" + testsFile, "formatter=" + XMLJUnitResultFormatter.class.getName() + ",xml", "showoutput=true"});
+  }
+
   public static void main(String[] args) throws Exception {
     TestType testType = TestType.valueOf(args[0]);
-    File testsFile = args.length == 1 ? prepareTestsFile(testType) : new File(args[1]);
-
-    // use ant's runner to output results into xml
-    JUnitTestRunner.main(new String[]{"testsfile=" + testsFile, "formatter=" + XMLJUnitResultFormatter.class.getName() + ",xml", "showoutput=true"});
+    int exitCode = new JUnitRunnerWithXMLOutput(testType).runTests();
+    System.exit(exitCode);
   }
 }
