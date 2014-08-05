52197e19813479ed3b34f161721a4e1609bdf293
Andrei Solntsev <andrei.solntsev@gmail.com>
2014-08-06 00:47:34 +0300

    log Play.mode Play.configuration before every test

--- src/play/test/PlayContextTestInvoker.java
+++ src/play/test/PlayContextTestInvoker.java
@@ -4,16 +4,24 @@ import org.junit.rules.MethodRule;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.Statement;
 import play.Invoker;
+import play.Play;
 import play.exceptions.UnexpectedException;
 
 public class PlayContextTestInvoker implements MethodRule {
-  @Override public Statement apply(final Statement base, FrameworkMethod method, Object target) {
+  @Override public Statement apply(final Statement base, final FrameworkMethod method, Object target) {
 
     return new Statement() {
 
       @Override
       public void evaluate() throws Throwable {
         try {
+          System.out.println(
+              "Before " + method.getMethod().getDeclaringClass() + '.' + method.getName() +
+              ": Play.mode=" + Play.mode +
+              ", Play.configuration.size=" + Play.configuration.size() +
+              ", evolutions.enabled=" + Play.configuration.getProperty("evolutions.enabled")
+          );
+          
           Invoker.invokeInThread(new Invoker.DirectInvocation() {
 
             @Override
